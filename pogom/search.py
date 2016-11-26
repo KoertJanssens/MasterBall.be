d77d843ecd02239c273471f3baf5ec001de66ad4
KoertJanssens <koert.janssens@gmail.com>
2016-11-26 15:40:26 +0100

    stagger thread delay to random between 0 and 10

--- pogom/search.py
+++ pogom/search.py
@@ -781,7 +781,7 @@ def calc_distance(pos1, pos2):
 def stagger_thread(args, account):
     if args.accounts.index(account) == 0:
         return  # No need to delay the first one.
-    delay = args.accounts.index(account) + ((random.random() - .5) / 2)
+    delay = 10 * random.random()
     log.debug('Delaying thread startup for %.2f seconds', delay)
     time.sleep(delay)
 
