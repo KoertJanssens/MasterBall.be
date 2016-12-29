f1c44edc1562a69ebd875b41ea70043fc0bb6c6e
KoertJanssens <koert.janssens@gmail.com>
2016-12-29 14:27:15 +0100

    0.51 bugfixes

--- pogom/search.py
+++ pogom/search.py
@@ -499,9 +499,9 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                 api = FakePogoApi(args.mock)
             else:
                 api = PGoApi()
-		    if args.hash_token:
-			api.activate_hash_server(args.hash_key)
-                        log.info('Created API instance using hash server.')
+		if args.hash_key:
+		    api.activate_hash_server(args.hash_key)
+                    log.info('Created API instance using hash server.')
 
             # New account - new proxy
             if args.proxy:
