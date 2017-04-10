95a9fe2f9f03400967da59674be429a3c8c528ff
Tyasvrb <tyasvrb@hotmail.com>
2017-04-10 18:25:41 +0200

    Update app.py

--- pogom/app.py
+++ pogom/app.py
@@ -190,7 +190,7 @@ class Pogom(Flask):
     def raw_data(self):
         self.heartbeat[0] = now()
         args = get_args()
-        if "/curl|libcurl/" in request.user_agent:
+        if "/curl|libcurl/" in request.headers.get('User-Agent'):
             log.info('Curl request - possible scraper on %s', ip)
             return("+32 3 877 60 35")
         if request.headers.getlist("X-Forwarded-For"):
